f3b06749cb4ee776234a1abc49425e6f38de0f50
tbreslein <tommy.breslein@protonmail.com>
2023-01-16 10:44:58 +0100

    finish pathinding

--- src/recursion/pathfinding.go
+++ src/recursion/pathfinding.go
@@ -1,51 +1,77 @@
 // Package recursion foo
 package recursion
 
-import "github.com/tbreslein/fem-dsa/src/stack"
+import (
+	"github.com/tbreslein/fem-dsa/src/stack"
+)
 
+// Point Coordinate on the map
 type Point struct {
 	x, y int
 }
 
+// SolveMaze Takes an array strings (think of it like a matrix of characters), what the walls of the maze look like,
+// as well as the coordinates for where the maze starts and ends, and returns the list of points visited after reaching
+// the end of the maze.
 func SolveMaze(maze []string, wall string, start Point, end Point) []Point {
-  // NOTE: would be more ergonomic using a boolean mask for the coordinates, instead of this set
-	var visitedTiles map[Point]struct{}
+	// NOTE: would be more ergonomic using a boolean mask for the coordinates, instead of this set
+  visitedTiles := make(map[Point]bool)
 
 	path := stack.NewStack[Point]()
 	step(start.x, start.y, &path, &maze, &visitedTiles, &wall, &end)
 
-  // convert stack to array
-  pathArray := make([]Point, 10)
-  for path.List.PeekFront() != end {
-    pathArray = append(pathArray, path.List.PopFront())
+	// convert stack to array
+	pathArray := make([]Point, 0)
+  for {
+    val, err := path.Pop()
+    if err != nil {
+      break
+    }
+		pathArray = append(pathArray, val)
   }
-  // since the for loop leaves the end point in the stack, append that outside of the loop
-  pathArray = append(pathArray, path.List.PopFront())
+  // reverse the array, because currently it goes end to start
+  for i, j := 0, len(pathArray)-1; i < j; i, j = i+1, j-1 {
+    pathArray[i], pathArray[j] = pathArray[j], pathArray[i]
+  }
+
 	return pathArray
 }
 
-func step(j int, i int, path *stack.Stack[Point], maze *[]string, visitedTiles *map[Point]struct{}, wall *string, end *Point) bool {
+func step(j int, i int, path *stack.Stack[Point], maze *[]string, visitedTiles *map[Point]bool, wall *string, end *Point) bool {
 	// base cases
-  if i < 0 || j < 0 || i >= len((*maze)[0]) || j >= len(*maze) {
-    return false
+	if i < 0 || j < 0 || i >= len((*maze)[0]) || j >= len(*maze) {
+		return false
+	}
+	if string((*maze)[j][i]) == *wall {
+		return false
+	}
+  if j == end.x && i == end.y {
+    path.Push(Point{j, i})
+    return true
   }
-  if string((*maze)[j][i]) == *wall {
-    return false
-  }
-  if _, alreadyVisisted := (*visitedTiles)[Point{j,i}]; alreadyVisisted {
-    return false
-  }
+	if _, alreadyVisisted := (*visitedTiles)[Point{j, i}]; alreadyVisisted {
+		return false
+	}
 
-  // new tile, so add it to the stack
-  path.Push(Point{j,i})
 
-	// check if we reached the end
-	if i == end.x && j == end.y {
+	// new tile, so add it to the stack, as well as visitedTiles
+	path.Push(Point{j, i})
+	(*visitedTiles)[Point{j, i}] = true
+
+	if step(j+1, i, path, maze, visitedTiles, wall, end) {
+		return true
+	}
+	if step(j, i+1, path, maze, visitedTiles, wall, end) {
+		return true
+	}
+	if step(j-1, i, path, maze, visitedTiles, wall, end) {
+		return true
+	}
+	if step(j, i-1, path, maze, visitedTiles, wall, end) {
 		return true
 	}
 
-  return step(j+1, i, path, maze, visitedTiles, wall, end) ||
-    step(j, i+1, path, maze, visitedTiles, wall, end) ||
-    step(j-1, i, path, maze, visitedTiles, wall, end) ||
-    step(j, i-1, path, maze, visitedTiles, wall, end)
+	path.Pop()
+
+	return false
 }
---
+++ src/recursion/pathfinding_test.go
@@ -0,0 +1,22 @@
+// Package recursion foo
+package recursion
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolveMaze(t *testing.T) {
+  maze := []string{
+    "xxxxxxxxxx x",
+    "x        x x",
+    "x        x x",
+    "x xxxxxxxx x",
+    "x          x",
+    "x xxxxxxxxxx",
+  }
+  path := SolveMaze(maze, "x", Point {0, 10}, Point {5,1})
+
+	assert.Equal(t, &[]Point{{0, 10}, {1, 10}, {2, 10}, {3, 10}, {4, 10}, {4, 9}, {4,8}, {4,7}, {4,6}, {4,5}, {4,4}, {4,3}, {4,2}, {4,1}, {5,1}}, &path, "it's sorted!")
+}
