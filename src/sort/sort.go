c619615ba2817dd76b299db8bf24b148ec09b98a
tbreslein <tommy.breslein@protonmail.com>
2023-01-16 15:23:24 +0100

    add quicksort

--- src/sort/sort.go
+++ src/sort/sort.go
@@ -5,14 +5,47 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// BubbleSort as the name suggests...
 func BubbleSort[T constraints.Ordered](arr *[]T) {
-  for i := 0; i < len(*arr); i++ {
-    for j := 0; j < len(*arr) - i - 1; j++ {
-      if (*arr)[j] > (*arr)[j+1] {
-        temp := (*arr)[j+1]
-        (*arr)[j+1] = (*arr)[j]
-        (*arr)[j] = temp
-      }
-    }
-  }
+	for i := 0; i < len(*arr); i++ {
+		for j := 0; j < len(*arr)-i-1; j++ {
+			if (*arr)[j] > (*arr)[j+1] {
+				swap(arr, j, j+1)
+			}
+		}
+	}
+}
+
+// QuickSort as the name suggests...
+func QuickSort[T constraints.Ordered](arr *[]T) {
+	qs(arr, 0, len(*arr)-1)
+}
+
+func qs[T constraints.Ordered](arr *[]T, lo int, hi int) {
+	if lo >= hi {
+		return
+	}
+	pivotIdx := partition(arr, lo, hi)
+	qs(arr, lo, pivotIdx-1)
+	qs(arr, pivotIdx+1, hi)
+}
+
+func partition[T constraints.Ordered](arr *[]T, lo int, hi int) int {
+	pivot := (*arr)[hi]
+	idx := lo - 1
+	for i := lo; i < hi; i++ {
+		if (*arr)[i] <= pivot {
+			idx++
+			swap(arr, idx, i)
+		}
+	}
+	idx++
+	swap(arr, hi, idx)
+	return idx
+}
+
+func swap[T any](arr *[]T, i int, j int) {
+	tmp := (*arr)[i]
+	(*arr)[i] = (*arr)[j]
+	(*arr)[j] = tmp
 }
--- src/sort/sort_test.go
+++ src/sort/sort_test.go
@@ -11,3 +11,9 @@ func TestBubbleSort(t *testing.T) {
 	BubbleSort(&foo)
 	assert.Equal(t, &foo, &[]int{1, 2, 3, 4, 7}, "it's sorted!")
 }
+
+func TestQuickSort(t *testing.T) {
+	foo := []int{1, 3, 7, 4, 2}
+	QuickSort(&foo)
+	assert.Equal(t, &foo, &[]int{1, 2, 3, 4, 7}, "it's sorted!")
+}
